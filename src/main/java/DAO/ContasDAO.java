497fb133da79db446aca5044362ba769ebefd89f
mablds <marcelobragalemos@gmail.com>
2020-06-09 23:59:47 -0300

    update create account routes and search for one account

--- src/main/java/DAO/ContasDAO.java
+++ src/main/java/DAO/ContasDAO.java
@@ -9,10 +9,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Conta;
+import model.ContaJuridica;
+import model.ContaPessoaFisica;
+import model.ContaPoupanca;
 import utils.GerenciadorConexao;
 
 /**
@@ -20,7 +27,50 @@ import utils.GerenciadorConexao;
  * @author Sakemi
  */
 public class ContasDAO {
+    public static Conta cadastrar(Conta newConta) {
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+        ResultSet rs = null;
+        Date now = new Date();
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO contas "
+                    + "(id_cliente, criacao, saldo, tipo, ativo) "
+                    + "VALUES(?, ?, ?, ?, ?);",
+                    Statement.RETURN_GENERATED_KEYS);
 
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            
+            instrucaoSQL.setInt(1, newConta.getCliente());
+            instrucaoSQL.setString(2, formatter.format(now));
+            instrucaoSQL.setDouble(3, newConta.getSaldo());
+            instrucaoSQL.setInt(4, newConta.getTipo());
+            instrucaoSQL.setBoolean(5, newConta.isAtiva());
+                        
+            instrucaoSQL.executeUpdate();
+
+            rs = instrucaoSQL.getGeneratedKeys();
+            rs.next();
+            
+            int contaId = rs.getInt(1);
+            
+            newConta.setId(contaId);
+            
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            newConta = null;
+        } finally {
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+                GerenciadorConexao.fecharConexao();
+            } catch (SQLException ex) {
+            }
+        }
+        return newConta;
+    }
+    
     public static List<ContasDTO> listarContas() {
         List<ContasDTO> contas = new ArrayList<>();
         Connection conexao;
@@ -65,5 +115,71 @@ public class ContasDAO {
         return contas;
 
     }
+    
+    public static Conta buscarConta(int id) {
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+        Conta contaEncontrada = null;
+        
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            instrucaoSQL = conexao.prepareStatement("SELECT * FROM contas WHERE id = ?;");
+            instrucaoSQL.setInt(1, id);
 
+            ResultSet rs = instrucaoSQL.executeQuery();
+            
+            while(rs.next()){
+                int idConta = rs.getInt("id"); //id_cliente, criacao, saldo, tipo, ativo
+                int idClienteConta = rs.getInt("id_cliente");
+                String criacao = rs.getString("criacao");
+                double saldo = rs.getDouble("saldo");
+                int tipo = rs.getInt("tipo");
+                boolean ativo = rs.getBoolean("ativo");
+
+                switch(tipo) {
+                    case 1:
+                        contaEncontrada = new ContaPessoaFisica(idConta, idClienteConta, criacao, tipo, saldo, ativo);
+                        break;
+                    case 2:
+                        contaEncontrada = new ContaJuridica(idConta, idClienteConta, criacao, tipo, saldo, ativo);
+                        break;
+                    case 3:
+                        contaEncontrada = new ContaPoupanca(idConta, idClienteConta, criacao, tipo, saldo, ativo);
+                        break;
+                }
+            }
+            
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+                GerenciadorConexao.fecharConexao();
+            } catch (SQLException ex) {
+            } finally {
+                try {
+                    if (instrucaoSQL != null) {
+                        instrucaoSQL.close();
+                    }
+                    GerenciadorConexao.fecharConexao();
+                } catch (SQLException ex) {
+                }
+            }
+        }
+        return contaEncontrada;
+    }
+    
+    public static Conta alterar(Conta updateConta) {
+        return updateConta;
+    } 
+    
+    public static boolean deletar(Conta deleteConta) {
+        return true;
+    }
+    
+    public static Conta ativar(Conta ativarConta) {
+        return ativarConta;
+    }
 }
--- src/main/java/poo/simplebankpoo/ContasController.java
+++ src/main/java/poo/simplebankpoo/ContasController.java
@@ -6,9 +6,17 @@
 package poo.simplebankpoo;
 
 import DAO.ContasDAO;
-import DTO.ClienteDTO;
+import DTO.ContasDTO;
 import java.util.List;
+import model.Conta;
+import model.ContaJuridica;
+import model.ContaPessoaFisica;
+import model.ContaPoupanca;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,9 +27,50 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContasController {
 
     @GetMapping("/contas")
-    public List<ClienteDTO> contas() {
+    public List<ContasDTO> contas() {
         List contas = ContasDAO.listarContas();
         return contas;
     }
 
+    @GetMapping("/conta/{id}")
+    public Conta conta(@PathVariable int id) {
+        Conta conta = ContasDAO.buscarConta(id);
+        return conta;
+    }
+
+    @PostMapping("/conta/create/pf") // pessoa fisica
+    public Conta createContaPf(@RequestBody ContaPessoaFisica newConta) {
+        Conta clienteCreated = ContasDAO.cadastrar(newConta);
+        return clienteCreated;
+    }
+    
+    @PostMapping("/conta/create/pj") //pessoa juridica
+    public Conta createContaPj(@RequestBody ContaJuridica newConta) {
+        Conta clienteCreated = ContasDAO.cadastrar(newConta);
+        return clienteCreated;
+    }
+    
+    @PostMapping("/conta/create/pp") //poupanca
+    public Conta createContaPp(@RequestBody ContaPoupanca newConta) {
+        Conta clienteCreated = ContasDAO.cadastrar(newConta);
+        return clienteCreated;
+    }
+    
+    @PutMapping("/conta/update")
+    public Conta updateConta(@RequestBody Conta updateConta) {
+        Conta clienteUpdated = ContasDAO.alterar(updateConta);
+        return clienteUpdated;
+    }
+    
+    @PutMapping("/conta/delete") //Fizemos como PUT porque apenas iremos alterar a propriedade de ativo no banco para false.
+    public boolean deleteConta(@RequestBody Conta deleteConta) {
+        boolean clienteDeleted = ContasDAO.deletar(deleteConta);
+        return clienteDeleted;
+    }
+    
+    @PutMapping("/conta/ativar") //Fizemos como PUT porque apenas iremos alterar a propriedade de ativo no banco para true.
+    public Conta ativarConta(@RequestBody Conta ativeConta) {
+        Conta clienteActivated = ContasDAO.ativar(ativeConta);
+        return clienteActivated;
+    }
 }
