950cca0aae55abd3615e57da928465d32503e815
mablds <marcelobragalemos@gmail.com>
2020-06-11 02:13:43 -0300

    add Env java

---
+++ src/main/java/utils/Env.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Env {
+    public static String USER = "uepdyzuiz1pmobm8";
+    public static String PASSWORD = "lrNMwvROAe5LAtyJeced";
+    public static String URL = "jdbc:mysql://uepdyzuiz1pmobm8:lrNMwvROAe5LAtyJeced@bdxwaxltcmvoabaaybib-mysql.services.clever-cloud.com:3306/bdxwaxltcmvoabaaybib?useSSL=false";
+}
